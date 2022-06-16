7b5d01b51f8cb6c830975ff0aac5ad512a4a5198
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-15 22:09:12 -0300

    useAuth: ADDED

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+export const useAuth = ( token ) => {
+  const [{ auth, loading }, setAuth] = useState({
+    auth: false,
+    loading: true
+  });
+
+
+  useEffect(() => {
+    if (token) {
+      setAuth({
+        auth: true,
+        loading: false
+      });
+    } else {
+      setAuth({
+        auth: false,
+        loading: false
+      });
+    }
+  }, [])
+
+  return [auth, loading];
+}
