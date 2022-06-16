c28c3366ba25ad42d60bc55bfedcccd8e9362f8d
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-15 22:08:44 -0300

    AuthContext & authReducer: ADDED

---
+++ src/context/AuthContext/AuthContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useReducer, useState } from "react";
+
+/**
+ * @helpers
+ */
+import { authReducer } from "./authReducer";
+import { types } from "../../types/types";
+import { useAuth } from "../../hooks/useAuth";
+
+/**
+ * @AuthContext
+ */
+export const AuthContext = createContext();
+
+/**
+ * @reducerInit
+ */
+const initReducer = () => {
+  return (localStorage.getItem("user")) ? localStorage.getItem("user") : null;
+}
+
+export const AuthProvider = ({ children }) => {
+  const [ user, dispatch ]  = useReducer(authReducer, {}, initReducer);
+  const [ token, setToken ] = useState(localStorage.getItem("token") || null);
+  const [ auth, loading ]   = useAuth(token);
+
+  const login = (token, user) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    dispatch({ type: types.LOGIN, payload: user });
+  }
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    dispatch({ type: types.LOGOUT });
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      user,
+      auth,
+      loading,
+      login,
+      logout
+    }}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
