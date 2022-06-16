f7ea53e31aa95b43e12ae44f204c8bf0ec55bdd1
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-15 22:47:20 -0300

    EvlunApp.jsx: ADDED

--- src/EvlunApp.jsx
+++ src/EvlunApp.jsx
@@ -1,4 +1,36 @@
-export const EvlunApp = () => {
+import { useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-  return <h1>EvlunApp</h1>
+/**
+ * @context
+ */
+import { AuthContext } from './context/AuthContext/AuthContext';
+
+/**
+ * @pages
+ */
+import { MainPage } from './pages/MainPage/MainPage';
+import { SignupPage } from './pages/SignupPage/SignupPage';
+import { LoginPage } from './pages/LoginPage/LoginPage';
+
+export const EvlunApp = () => {
+  const { auth, loading } = useContext(AuthContext);
+  
+  return (
+    <>
+      {
+        !loading && (
+          <Router>
+            <Routes>
+
+              <Route path="/" element={ (auth) ? <MainPage /> : <LoginPage /> } />
+              <Route path="/signup" element={ (auth) ? <Navigate to='/' /> : <SignupPage /> } />
+              <Route path="/login" element={ (auth) ? <Navigate to='/' /> : <LoginPage /> } />
+
+            </Routes>
+          </Router>
+        )
+      }
+    </>
+  )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { AuthProvider } from './context/AuthContext/AuthContext';
 import { EvlunApp } from './EvlunApp'
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
     <EvlunApp />
+  </AuthProvider>
 )
