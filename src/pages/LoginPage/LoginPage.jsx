d50d10e468f228a546a20223a885617d6c9e5baf
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-17 12:03:27 -0300

    page LoginPage: ADDED

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,69 @@
-export const LoginPage = () => {
+/**
+ * @hooks
+ */
+ import { useForm } from '../../hooks/useForm'
 
-  return <h1>Login Page</h1>;
-}
+ /**
+  * @components
+  */
+ import { Main } from '../../components/Main/Main'
+ import { Container } from '../../components/Container/Container'
+ import { Form, FormInputPrimary, FormButtonPrimary, FormQuestion } from '../../components/Form'
+ 
+ /**
+  * @styles
+  */
+ import styles from './loginPage.module.scss';
+ 
+ export const LoginPage = () => {
+   const [ { email, password }, handleInputChange ] = useForm({
+     email: '',
+     password: ''
+   });
+ 
+   const onSubmit = ( event ) => {
+     event.preventDefault();
+   }
+ 
+   return (
+     <Main>
+       <Container>
+ 
+         <p className={styles.form__legend}>
+           Iniciar sesión en <span>Evlun</span>
+         </p>
+ 
+         <Form
+           autocomplete='off'
+           formSubmit={onSubmit}
+         >
+           <FormInputPrimary 
+             inputName='email'
+             label='Correo electronico'
+             inputValue={email}
+             inputChange={handleInputChange}
+             inputType='email'
+           />
+           <FormInputPrimary 
+             inputName='password'
+             label='Contraseña'
+             inputValue={password}
+             inputChange={handleInputChange}
+             inputType='password'
+           />
+           <FormButtonPrimary 
+             buttonText='Iniciar Sesión'
+             buttonSubmit={onSubmit}
+           />
+         </Form>
+ 
+         <FormQuestion 
+           question='¿No tienes una cuenta?'
+           linkQuestion='Regístrate'
+           link='/signup'
+         />
+         
+       </Container>
+     </Main>
+   )
+ }
