1c5285ad637ac25663c1a0e1bd689b1bf2330430
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-20 15:54:27 -0300

    fixture data: ADDED

---
+++ src/fixtures/user.js
@@ -0,0 +1,8 @@
+export const user = {
+  name: 'Tomas Cuevas',
+  username: '@Tomas_Cuevas15',
+  email: 'tomascuevas@gmail.com',
+  avatar: 'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png',
+  following: '80',
+  followers: '325'
+}
