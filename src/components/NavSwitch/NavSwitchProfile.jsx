1164c73484c2f461b7d2ebd52489087c0db3dcdb
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-20 16:18:54 -0300

    component NavSwitchProfile.jsx: ADDED

---
+++ src/components/NavSwitch/NavSwitchProfile.jsx
@@ -0,0 +1,54 @@
+import { useContext } from "react"
+
+/**
+ * @styles
+ */
+import styles from './navSwitchProfile.module.scss';
+
+/**
+ * @context
+ */
+import { AuthContext } from "../../context/AuthContext/AuthContext";
+
+export const NavSwitchProfile = () => {
+  const { user } = useContext(AuthContext);
+  const { avatar, name, username, following, followers } = user;
+
+  return (
+    <section className={styles.profile}>
+      <div className={styles.profile__top}>
+        <img 
+          src={avatar} 
+          alt='profile-img' 
+          className={styles.profile__img} 
+        />
+      </div>
+
+      <div className={styles.profile__medium}>
+        <div className={styles.profile__info}>
+          <p className={styles.name}>
+            { name }
+          </p>
+        </div>
+        <div className={styles.profile__info}>
+          <p className={styles.username}>
+            { username }
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.profile__bottom}>
+        <div className={styles.profile__numbers_container}>
+          <p className={styles.profile__numbers}>
+            <span>{ following }</span> Siguiendo
+          </p>
+        </div>
+        <div className={styles.profile__numbers_container}>
+          <p className={styles.profile__numbers}>
+            <span>{ followers }</span> Seguidores
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
