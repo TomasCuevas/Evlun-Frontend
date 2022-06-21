7ff56436bfef43277c672027d0542054f8d10c55
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-21 19:58:26 -0300

    component NavSwitchBottom.jsx: ADDED

---
+++ src/components/NavSwitch/NavSwitchBottom.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+/**
+ * @styles
+ */
+import styles from './navSwitchBottom.module.scss';
+
+export const NavSwitchBottom = () => {
+
+  return (
+    <section className={styles.bottom}>
+      <div className={styles.logout__container}>
+        <Link to='/logout'>
+          <span className={styles.text}>Cerrar Sesión</span>
+        </Link>
+      </div>
+    </section>
+  )
+}
