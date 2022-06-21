32255375b995c0de3bce1d2e7191362daf6df115
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-21 19:42:23 -0300

    page MainPage.jsx UPDATED: components ADDED to the page & ADDED context

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,51 @@
-export const MainPage = () => {
+import { useEffect, useContext } from "react"
 
-  return <h1>Main Page</h1>;
+/**
+ * @components
+ */
+import { Main } from "../../components/Main/Main"
+
+/**
+ * layouts
+ */
+import { Nav } from "../../layouts/Nav/Nav"
+import { NavSwitch } from "../../layouts/NavSwitch/NavSwitch"
+import { NavTopHome } from "../../layouts/NavTopHome/NavTopHome"
+
+/**
+ * @provider
+ */
+import { MainProvider } from "../../context/MainContext/MainContext"
+
+/**
+ * @context
+ */
+import { AuthContext } from "../../context/AuthContext/AuthContext"
+
+/**
+ * @fixtureData
+ */
+import { user } from "../../fixtures/user"
+
+export const MainPage = () => {
+  const { login } = useContext(AuthContext);
+
+  useEffect(() => {
+    const onLogin = async() => {
+      login('abc123', user);
+    }
+    
+    onLogin();
+      
+  }, [])
+
+  return (
+    <MainProvider>
+      <Main>
+        <NavTopHome />
+        <Nav />
+        <NavSwitch />
+      </Main>
+    </MainProvider>
+  )
 }
