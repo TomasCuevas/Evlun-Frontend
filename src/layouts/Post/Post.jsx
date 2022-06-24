bed5e24dbfada79b0c2f12c6104394109d51037a
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 22:30:32 -0300

    layout Post: ADDED

---
+++ src/layouts/Post/Post.jsx
@@ -0,0 +1,36 @@
+/**
+ * @components
+ */
+import { PostAvatarSection } from '../../components/Post/PostAvatarSection';
+import { PostBottom } from '../../components/Post/PostBottom';
+import { PostContent } from '../../components/Post/PostContent';
+import { PostTop } from '../../components/Post/PostTop';
+
+/**
+ * @styles
+ */
+import styles from './post.module.scss';
+
+export const Post = ({ post }) => {
+
+  return (
+    <article className={styles.post__container}>
+      
+      <PostAvatarSection 
+        avatar={ post.added_by.avatar }
+        name={ post.added_by.name }
+      />
+
+      <div className={styles.post__section}>
+        <PostTop 
+          name={ post.added_by.name }
+          username={ post.added_by.username }
+          date={ post.date }
+        />
+        <PostContent content={ post.content } />
+        <PostBottom />
+      </div>
+
+    </article>
+  )
+}
