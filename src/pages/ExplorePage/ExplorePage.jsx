61954e12e5ac64d558195327210e0d62e07bf931
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-24 18:44:16 -0300

    page ExplorePage: ADDED

---
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -0,0 +1,30 @@
+/**
+ * @components
+ */
+import { Main } from "../../components/Main/Main"
+
+/**
+ * @layouts
+ */
+import { Nav } from "../../layouts/Nav/Nav"
+import { NavSwitch } from "../../layouts/NavSwitch/NavSwitch"
+import { NavTopExplore } from "../../layouts/NavTopExplore/NavTopExplore"
+
+/**
+ * @providers
+ */
+import { NavSwitchProvider } from "../../context/NavSwitchContext/NavSwitchContext"
+
+export const ExplorePage = () => {
+
+  return (
+    <NavSwitchProvider>
+      <NavTopExplore />
+      <Nav />
+      <NavSwitch />
+      <Main>
+
+      </Main>
+    </NavSwitchProvider>
+  )
+}
