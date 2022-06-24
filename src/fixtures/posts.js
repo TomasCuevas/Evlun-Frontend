c308fc896e1add192830f829f3df0f29a73d4bce
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 22:21:11 -0300

    fixture data ADDED: posts

---
+++ src/fixtures/posts.js
@@ -0,0 +1,50 @@
+export const posts = [
+  { 
+    id: 1, 
+    content: 'Exercitation labore amet enim reprehenderit laboris ullamco.', 
+    date: 1656010117427, 
+    likes: 5, 
+    comments: 1, 
+    added_by: { 
+      name: 'Tomas Cuevas',
+      username: '@Tomas_Cuevas15',
+      avatar: 'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png' 
+    } 
+  },
+  { 
+    id: 2, 
+    content: 'Incididunt officia culpa tempor cupidatat amet tempor eu aliqua.', 
+    date: 1656010540048, 
+    likes: 13, 
+    comments: 4, 
+    added_by: { 
+      name: 'Alberto Fernandez',
+      username: '@albertito1415',
+      avatar: 'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png' 
+    } 
+  },
+  { 
+    id: 3, 
+    content: 'Culpa id enim labore do aute ut qui fugiat ex aliqua sit. Anim minim id enim Lorem ea eu irure sit id. Aliquip in commodo voluptate ex. Exercitation aliqua ea dolor non adipisicing reprehenderit nulla proident id.', 
+    date: 1656030500031, 
+    likes: 3, 
+    comments: 0, 
+    added_by: { 
+      name: 'Leo Messi',
+      username: '@leomessi',
+      avatar: 'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png' 
+    } 
+  },
+  { 
+    id: 4, 
+    content: 'Do in labore consequat.', 
+    date: new Date().getTime(), 
+    likes: 42, 
+    comments: 12, 
+    added_by: { 
+      name: 'Gatito Fuerte',
+      username: '@gatito33',
+      avatar: 'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png' 
+    } 
+  }
+]
