1d592c3f21dbc58cb3cac5cfeb069edeced9bd34
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 22:24:54 -0300

    component PostBottom.jsx: ADDED

---
+++ src/components/Post/PostBottom.jsx
@@ -0,0 +1,23 @@
+/**
+ * @icons
+ */
+import { MdOutlineChatBubbleOutline, MdOutlineFavoriteBorder, MdOutlineFavorite } from 'react-icons/md';
+
+/**
+ * @styles
+ */
+import styles from './postBottom.module.scss';
+
+export const PostBottom = () => {
+
+  return (
+    <div className={styles.post__bottom}>
+      <div className={styles.comment__icon_container}>
+        <MdOutlineChatBubbleOutline />
+      </div>
+      <div className={styles.favorite__icon_container}>
+        <MdOutlineFavoriteBorder />
+      </div>
+    </div>
+  )
+}
