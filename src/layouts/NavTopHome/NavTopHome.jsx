8451f13699a12a0d09973665d58b88d2571aa6fb
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-24 18:40:44 -0300

    layout NavTopHome UPDATED: jsx modified

--- src/layouts/NavTopHome/NavTopHome.jsx
+++ src/layouts/NavTopHome/NavTopHome.jsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
  * @components
  */
 import { Container } from '../../components/Container/Container';
+import { NavTopAvatar } from '../../components/NavTop/NavTopAvatar';
 
 /**
  * @styles
@@ -14,37 +15,24 @@ import styles from './navTopHome.module.scss';
  * context
  */
 import { AuthContext } from '../../context/AuthContext/AuthContext';
-import { MainContext } from '../../context/MainContext/MainContext';
 
 export const NavTopHome = () => {
   const { user } = useContext(AuthContext);
-  const { onNavSwitch } = useContext(MainContext);
 
   const { avatar } = user;
   
   return (
-    <>
-      <div className={styles.container__all}>
-      
-        <Container>
-          <nav className={styles.nav__container}>
-            <div 
-              className={styles.avatar__container}
-              onClick={() => onNavSwitch(true)}
-            >
-              <img 
-                src={avatar} 
-                alt='profile-img' 
-                className={styles.avatar__img}
-              />
-            </div>
-            <div className={styles.newposts__container}>
-              <p className={styles.newposts}>Posts más recientes</p>
-            </div>
-          </nav>
-        </Container>
+    <div className={styles.container__all}>
+    
+      <Container>
+        <nav className={styles.nav__container}>
+          <NavTopAvatar avatar={ avatar } />
+          <div className={styles.newposts__container}>
+            <p className={styles.newposts}>Posts más recientes</p>
+          </div>
+        </nav>
+      </Container>
 
-      </div>
-    </>
+    </div>
   )
 }
