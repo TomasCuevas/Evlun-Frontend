a66a7ad4ab065e20c1660394f922c7adad55f9cb
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-26 16:55:18 -0300

    component NameUsername.jsx: ADDED

---
+++ src/components/ProfileHero/NameUsername.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+
+/**
+ * @styles
+ */
+import Styles from "./nameUsername.module.scss";
+
+/**
+ * @context
+ */
+import { AuthContext } from "../../context/AuthContext/AuthContext";
+
+export const NameUsername = () => {
+  const { name, username } = useContext(AuthContext);
+
+  return (
+    <div className={Styles.info__user_container}>
+      <span className={Styles.name}>{name}</span>
+      <span className={Styles.username}>{username}</span>
+    </div>
+  );
+};
