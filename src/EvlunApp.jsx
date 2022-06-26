9082e7b72725fbe7dbc9e605732cea392a69d177
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-26 17:10:09 -0300

    EvlunApp.jsx: ADDED new routes & provider context

--- src/EvlunApp.jsx
+++ src/EvlunApp.jsx
@@ -7,12 +7,18 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext/AuthContext';
 
 /**
+ * @providers
+ */
+import { ProfileProvider } from './context/ProfileContext/ProfileContext';
+
+/**
  * @pages
  */
 import { MainPage } from './pages/MainPage/MainPage';
 import { SignupPage } from './pages/SignupPage/SignupPage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { ExplorePage } from './pages/ExplorePage/ExplorePage';
+import { ProfilePage } from './pages/ProfilePage/ProfilePage';
 
 export const EvlunApp = () => {
   const { auth, loading } = useContext(AuthContext);
@@ -27,6 +33,11 @@ export const EvlunApp = () => {
             <Route path='/signup' element={ (auth) ? <Navigate to='/' /> : <SignupPage /> } />
             <Route path='/login' element={ (auth) ? <Navigate to='/' /> : <LoginPage /> } />
             <Route path='/explore' element={ <ExplorePage /> } />
+            <Route path='/profile/:username' element={
+              <ProfileProvider>
+                <ProfilePage />
+              </ProfileProvider>
+            } />
             <Route path='*' element={ <Navigate to='/' /> } />
 
           </Routes>
