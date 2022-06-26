a7ae8c16f674f7b7feaa434020a33f29063a9c49
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-26 17:05:58 -0300

    layout NavTopExplore: UPDATED children

--- src/layouts/NavTopExplore/NavTopExplore.jsx
+++ src/layouts/NavTopExplore/NavTopExplore.jsx
@@ -1,36 +1,34 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
 /**
  * @components
  */
-import { Container } from '../../components/Container/Container';
-import { Explore } from '../../components/NavTopExplore/Explore';
-import { NavTopAvatar } from '../../components/NavTop/NavTopAvatar';
+import { Container } from "../../components/Container/Container";
+import { Explore } from "../../components/NavTop/Explore";
+import { Avatar } from "../../components/NavTop/Avatar";
 
 /**
  * @styles
  */
-import styles from './navTopExplore.module.scss';
+import styles from "./navTopExplore.module.scss";
 
 /**
  * context
  */
-import { AuthContext } from '../../context/AuthContext/AuthContext';
+import { AuthContext } from "../../context/AuthContext/AuthContext";
 
 export const NavTopExplore = () => {
   const { user } = useContext(AuthContext);
   const { avatar } = user;
-  
+
   return (
     <div className={styles.container__all}>
-    
       <Container>
         <nav className={styles.nav__container}>
-          <NavTopAvatar avatar={ avatar } />
+          <Avatar avatar={avatar} />
           <Explore />
         </nav>
       </Container>
-
     </div>
-  )
-}
+  );
+};
