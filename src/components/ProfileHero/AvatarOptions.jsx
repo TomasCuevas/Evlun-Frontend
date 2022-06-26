084fd52a74201e796cc6a7ccf406af8071b6757a
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-26 16:50:17 -0300

    component AvatarOptions.jsx: ADDED

---
+++ src/components/ProfileHero/AvatarOptions.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+/**
+ * @components
+ */
+import { Option } from "./Option";
+
+/**
+ * @styles
+ */
+import Styles from "./avatarOptions.module.scss";
+
+/**
+ * @context
+ */
+import { AuthContext } from "../../context/AuthContext/AuthContext";
+
+export const AvatarOptions = () => {
+  const { avatar } = useContext(AuthContext);
+
+  return (
+    <div className={Styles.info_top__container}>
+      <div className={Styles.avatar__container}>
+        <img src={avatar} alt="user img" className={Styles.avatar} />
+      </div>
+      <div className={Styles.options__container}>
+        <Option text="Editar Perfil" />
+      </div>
+    </div>
+  );
+};
