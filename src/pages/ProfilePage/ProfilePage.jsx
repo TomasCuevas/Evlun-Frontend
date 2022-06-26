627b044f551542fdf3dcf1296127b87f165e5c8c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-26 17:09:07 -0300

    page ProfilePage: ADDED

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,43 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+/**
+ * @components
+ */
+import { Main } from "../../components/Main/Main";
+
+/**
+ * @layouts
+ */
+import { Nav } from "../../layouts/Nav/Nav";
+import { NavTopProfile } from "../../layouts/NavTopProfile/NavTopProfile";
+import { ProfileHero } from "../../layouts/ProfileHero/ProfileHero";
+
+/**
+ * @provider
+ */
+import { NavSwitchProvider } from "../../context/NavSwitchContext/NavSwitchContext";
+
+/**
+ * @context
+ */
+import { ProfileContext } from "../../context/ProfileContext/ProfileContext";
+
+export const ProfilePage = () => {
+  const { onChangeUsername } = useContext(ProfileContext);
+  const { username } = useParams();
+
+  useEffect(() => {
+    onChangeUsername(username);
+  }, [username]);
+
+  return (
+    <NavSwitchProvider>
+      <Nav />
+      <NavTopProfile />
+      <Main>
+        <ProfileHero />
+      </Main>
+    </NavSwitchProvider>
+  );
+};
