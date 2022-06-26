0f344f5864733ea22b17ec03129fcfb633d77a9c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-26 17:07:02 -0300

    layout NavTopHome: UPDATED children & context

--- src/layouts/NavTopHome/NavTopHome.jsx
+++ src/layouts/NavTopHome/NavTopHome.jsx
@@ -1,38 +1,36 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
 /**
  * @components
  */
-import { Container } from '../../components/Container/Container';
-import { NavTopAvatar } from '../../components/NavTop/NavTopAvatar';
+import { Container } from "../../components/Container/Container";
+import { Avatar } from "../../components/NavTop/Avatar";
 
 /**
  * @styles
  */
-import styles from './navTopHome.module.scss';
+import styles from "./navTopHome.module.scss";
 
 /**
  * context
  */
-import { AuthContext } from '../../context/AuthContext/AuthContext';
+import { AuthContext } from "../../context/AuthContext/AuthContext";
 
 export const NavTopHome = () => {
   const { user } = useContext(AuthContext);
 
   const { avatar } = user;
-  
+
   return (
     <div className={styles.container__all}>
-    
       <Container>
         <nav className={styles.nav__container}>
-          <NavTopAvatar avatar={ avatar } />
+          <Avatar avatar={avatar} />
           <div className={styles.newposts__container}>
             <p className={styles.newposts}>Posts más recientes</p>
           </div>
         </nav>
       </Container>
-
     </div>
-  )
-}
+  );
+};
