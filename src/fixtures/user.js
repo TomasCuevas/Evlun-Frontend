ded90155c1cfe7b72a6b603427a2fb9d75320494
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-26 16:37:58 -0300

    fixture data: user UPDATED

--- src/fixtures/user.js
+++ src/fixtures/user.js
@@ -3,6 +3,10 @@ export const user = {
   username: '@Tomas_Cuevas15',
   email: 'tomascuevas@gmail.com',
   avatar: 'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png',
-  following: '80',
-  followers: '325'
+  banner: 'https://pbs.twimg.com/profile_banners/1539653081633378304/1655920356/1080x360',
+  description: 'Ullamco eu ea voluptate ad voluptate nisi duis duis consequat. Officia nisi veniam minim labore aute. Laboris reprehenderit sit ut dolor elit nisi enim.',
+  followings: '80',
+  followers: '325',
+  date: 1656010117427,
+  location: 'Argentina'
 }
