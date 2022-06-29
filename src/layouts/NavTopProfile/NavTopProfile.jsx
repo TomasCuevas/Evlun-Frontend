9334dabd6ca1dca331ec79fc4cb40a56efdfabf9
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-29 15:00:36 -0300

    layout NavTopProfile: MODIFIED

--- src/layouts/NavTopProfile/NavTopProfile.jsx
+++ src/layouts/NavTopProfile/NavTopProfile.jsx
@@ -1,22 +1,31 @@
+import { useContext } from 'react';
+
 /**
  * @components
  */
 import { Container } from '../../components/Container/Container';
 import { Back } from '../../components/NavTop/Back';
-import { UsernameCount } from '../../components/NavTop/UsernameCount';
+import { NavText } from '../../components/NavTop/NavText';
 
 /**
  * @styles
  */
 import Styles from './navTopProfile.module.scss';
 
+/**
+ * @context
+ */
+import { AuthContext } from '../../context/AuthContext/AuthContext';
+
 export const NavTopProfile = () => {
+  const { name, username } = useContext(AuthContext);
+
   return (
     <div className={Styles.container__all}>
       <Container>
         <div className={Styles.nav__container}>
           <Back />
-          <UsernameCount />
+          <NavText textBig={name} textSmall={username} />
         </div>
       </Container>
     </div>
