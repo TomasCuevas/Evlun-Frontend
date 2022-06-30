19a8b81730109a76a17d2df859e801efd3826043
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-30 15:41:55 -0300

    page SettingsPage: ADDED

---
+++ src/pages/SettingsPages/SettingsPage.jsx
@@ -0,0 +1,24 @@
+/**
+ * @components
+ */
+import { Main } from '../../components/Main/Main';
+import { SettingOption } from '../../components/Settings/SettingOption';
+
+/**
+ * @layouts
+ */
+import { Nav } from '../../layouts/Nav/Nav';
+import { NavTopSettings } from '../../layouts/NavTopSettings/NavTopSettings';
+
+export const SettingsPage = () => {
+  return (
+    <>
+      <NavTopSettings navText="Configuración" />
+      <Nav />
+      <Main>
+        <SettingOption optionTitle="Tu cuenta" navigateLink="/settings/account" />
+        <SettingOption optionTitle="Privacidad y seguridad" navigateLink="/settings/privacy" />
+      </Main>
+    </>
+  );
+};
