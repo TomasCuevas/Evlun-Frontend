e61621d14fd26251881b2b80b0b1e2b3125f0419
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-30 17:11:36 -0300

    multiple changes in EvlunApp and fixture data

--- src/EvlunApp.jsx
+++ src/EvlunApp.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 /**
@@ -18,30 +18,63 @@ import { ExplorePage } from './pages/ExplorePage/ExplorePage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { MainPage } from './pages/MainPage/MainPage';
 import { ProfilePage } from './pages/ProfilePage/ProfilePage';
-import { SettingsPage } from './pages/SettingsPage/SettingsPage';
+import { SettingsAccountPage } from './pages/SettingsPages/SettingsAccountPage';
+import { SettingsChangeYourPasswordPage } from './pages/SettingsPages/SettingsChangeYourPasswordPage';
+import { SettingsDeactivatePage } from './pages/SettingsPages/SettingsDeactivatePage';
+import { SettingsPage } from './pages/SettingsPages/SettingsPage';
+import { SettingsYourDataPage } from './pages/SettingsPages/SettingsYourDataPage';
 import { SignupPage } from './pages/SignupPage/SignupPage';
+
+/**
+ * @fixtureData
+ */
+import { user } from './fixtures/user';
 
 export const EvlunApp = () => {
   const { auth, loading } = useContext(AuthContext);
+
+  const { login } = useContext(AuthContext);
+
+  useEffect(() => {
+    const onLogin = async () => {
+      login('abc123', user);
+    };
+
+    onLogin();
+  }, [auth]);
 
   return (
     <>
       {!loading && (
         <Routes>
-          <Route path='/' element={auth ? <MainPage /> : <LoginPage />} />
-          <Route path='/signup' element={auth ? <Navigate to='/' /> : <SignupPage />} />
-          <Route path='/login' element={auth ? <Navigate to='/' /> : <LoginPage />} />
-          <Route path='/explore' element={<ExplorePage />} />
-          <Route path='/settings' element={<SettingsPage />} />
+          <Route path="/" element={auth ? <MainPage /> : <LoginPage />} />
+          <Route path="/signup" element={auth ? <Navigate to="/" /> : <SignupPage />} />
+          <Route path="/login" element={auth ? <Navigate to="/" /> : <LoginPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+
+          <Route path="/settings">
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/settings/account">
+              <Route path="/settings/account" element={<SettingsAccountPage />} />
+              <Route path="/settings/account/your_evlun_data" element={<SettingsYourDataPage />} />
+              <Route
+                path="/settings/account/password"
+                element={<SettingsChangeYourPasswordPage />}
+              />
+              <Route path="/settings/account/deactivate" element={<SettingsDeactivatePage />} />
+            </Route>
+          </Route>
+
           <Route
-            path='/profile/:username'
+            path="/profile/:username"
             element={
               <ProfileProvider>
                 <ProfilePage />
               </ProfileProvider>
             }
           />
-          <Route path='*' element={<Navigate to='/' />} />
+
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       )}
     </>
--- src/fixtures/user.js
+++ src/fixtures/user.js
@@ -2,14 +2,15 @@ export const user = {
   name: 'Tomas Cuevas',
   username: '@Tomas_Cuevas15',
   email: 'tomascuevas@gmail.com',
-  avatar:
-    'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png',
-  banner:
-    'https://pbs.twimg.com/profile_banners/1539653081633378304/1655920356/1080x360',
+  avatar: 'https://res.cloudinary.com/dn3kl3egc/image/upload/v1636226226/Avatar/avatar_default.png',
+  banner: 'https://pbs.twimg.com/profile_banners/1539653081633378304/1655920356/1080x360',
   description:
     'Ullamco eu ea voluptate ad voluptate nisi duis duis consequat. Officia nisi veniam minim labore aute. Laboris reprehenderit sit ut dolor elit nisi enim.',
   followings: '80',
   followers: '325',
   date: 1656010117427,
   location: 'Argentina',
+  country: 'Argentina',
+  phone: 3483531539,
+  gender: 'Hombre',
 };
