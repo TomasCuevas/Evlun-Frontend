f28c7fc6fd6a318324f012530b4a58acd7aafb26
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-04 15:48:09 -0300

    page SettingsCountryPage: ADDED

---
+++ src/pages/SettingsPages/SettingsCountryPage.jsx
@@ -0,0 +1,73 @@
+import { useContext, useEffect } from 'react';
+import { countries } from 'countries-list';
+
+/**
+ * @hooks
+ */
+import { useForm } from '../../hooks/useForm';
+
+/**
+ * components
+ */
+import { Container } from '../../components/Container/Container';
+import { Form, FormButtonPrimary } from '../../components/Form';
+import { FormSelectOption } from '../../components/Form/FormSelectOption';
+import { Main } from '../../components/Main/Main';
+
+/**
+ * @layouts
+ */
+import { Nav } from '../../layouts/Nav/Nav';
+import { NavTopSettings } from '../../layouts/NavTopSettings/NavTopSettings';
+
+/**
+ * @context
+ */
+import { AuthContext } from '../../context/AuthContext/AuthContext';
+
+export const SettingsCountryPage = () => {
+  const { country = '' } = useContext(AuthContext);
+  const { newCountry, onInputChange } = useForm({
+    newCountry: country,
+  });
+
+  const allCountries = [];
+  for (const country in countries) {
+    allCountries.push(countries[country].name);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    if (country === '') {
+      onInputChange({ target: { name: 'newCountry', value: 'Andorra' } });
+    }
+  }, [country]);
+
+  return (
+    <>
+      <NavTopSettings navText="Cambiar país" />
+      <Nav />
+      <Main>
+        <Container>
+          <Form formSubmit={onSubmit}>
+            <FormSelectOption
+              inputValue={newCountry}
+              inputName="newCountry"
+              optionValues={allCountries}
+              onInputChange={onInputChange}
+              label="País"
+            />
+            <FormButtonPrimary
+              buttonSubmit={onSubmit}
+              buttonText="Guardar"
+              option={country === newCountry}
+            />
+          </Form>
+        </Container>
+      </Main>
+    </>
+  );
+};
