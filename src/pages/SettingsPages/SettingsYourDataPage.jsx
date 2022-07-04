7bb7522221669e71265bb4e7038ac0d52c2a1092
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-04 15:49:36 -0300

    page SettingsYourDataPage: ADDED

--- src/pages/SettingsPages/SettingsYourDataPage.jsx
+++ src/pages/SettingsPages/SettingsYourDataPage.jsx
@@ -28,16 +28,28 @@ export const SettingsYourDataPage = () => {
       <NavTopSettings navText="Información de la cuenta" />
       <Nav />
       <Main>
-        <SettingOption optionTitle="Nombre de usuario" optionText={username} />
-        <SettingOption optionTitle="Teléfono" optionText={`${phone}` || null} />
-        <SettingOption optionTitle="Correo electrónico" optionText={email} />
+        <SettingOption
+          optionTitle="Nombre de usuario"
+          optionText={username}
+          navigateLink="/settings/screen_name"
+        />
+        <SettingOption
+          optionTitle="Teléfono"
+          optionText={`${phone}` || null}
+          navigateLink="/settings/phone"
+        />
+        <SettingOption
+          optionTitle="Correo electrónico"
+          optionText={email}
+          navigateLink="/settings/email"
+        />
         <SettingOption
           optionTitle="Creación de la cuenta"
           optionText={transformDate}
           arrow="none"
         />
-        <SettingOption optionTitle="País" optionText={country} />
-        <SettingOption optionTitle="Género" optionText={gender} />
+        <SettingOption optionTitle="País" optionText={country} navigateLink="/settings/country" />
+        <SettingOption optionTitle="Género" optionText={gender} navigateLink="/settings/gender" />
       </Main>
     </>
   );
