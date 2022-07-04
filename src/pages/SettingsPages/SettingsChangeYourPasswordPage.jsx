05245c3d71dbf3bffcbcfd3b6778d60edac26743
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-04 16:15:10 -0300

    page SettingsChangeYourPasswordPage: ADDED

--- src/pages/SettingsPages/SettingsChangeYourPasswordPage.jsx
+++ src/pages/SettingsPages/SettingsChangeYourPasswordPage.jsx
@@ -1,6 +1,13 @@
 /**
+ * @hooks
+ */
+import { useForm } from '../../hooks/useForm';
+
+/**
  * @components
  */
+import { Container } from '../../components/Container/Container';
+import { Form, FormButtonPrimary, FormInputPrimary } from '../../components/Form';
 import { Main } from '../../components/Main/Main';
 
 /**
@@ -10,11 +17,52 @@ import { Nav } from '../../layouts/Nav/Nav';
 import { NavTopSettings } from '../../layouts/NavTopSettings/NavTopSettings';
 
 export const SettingsChangeYourPasswordPage = () => {
+  const { oldPassword, newPassword, confirm, onInputChange } = useForm({
+    oldPassword: '',
+    newPassword: '',
+    confirm: '',
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <NavTopSettings navText="Cambia tu contraseña" />
       <Nav />
-      <Main></Main>
+      <Main>
+        <Container>
+          <Form formSubmit={onSubmit}>
+            <FormInputPrimary
+              inputChange={onInputChange}
+              inputName="oldPassword"
+              inputType="password"
+              inputValue={oldPassword}
+              label="Contraseña actual"
+            />
+            <FormInputPrimary
+              inputChange={onInputChange}
+              inputName="newPassword"
+              inputType="password"
+              inputValue={newPassword}
+              label="Nueva contraseña"
+            />
+            <FormInputPrimary
+              inputChange={onInputChange}
+              inputName="confirm"
+              inputType="password"
+              inputValue={confirm}
+              label="Confirmar contraseña"
+            />
+            <FormButtonPrimary
+              buttonSubmit={onSubmit}
+              buttonText="Guardar"
+              option={oldPassword.length < 8 || newPassword.length < 8 || confirm.length < 8}
+            />
+          </Form>
+        </Container>
+      </Main>
     </>
   );
 };
