dfd7c771572be961193cfa2e48019be08bff131c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-06 19:48:04 -0300

    /components/NavSwitch index.js: ADDED

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,6 @@
 export * from './Form';
 export * from './FormButtonPrimary';
+export * from './FormCheck';
 export * from './FormInputPrimary';
 export * from './FormQuestion';
 export * from './FormSelectOption';
