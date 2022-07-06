65365430d620c9b3c0119e28e40cd869a60c9c27
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-06 19:43:45 -0300

    component Container UPDATED: ADDED PropTypes and new property

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,8 +1,19 @@
+import PropTypes from 'prop-types';
+
 /**
  * @styles
  */
 import styles from './container.module.scss';
 
-export const Container = ({ children }) => {
-  return <div className={styles.container__all}>{children}</div>;
+export const Container = ({ children, mt = 0 }) => {
+  return (
+    <div style={{ marginTop: mt }} className={styles.container__all}>
+      {children}
+    </div>
+  );
+};
+
+Container.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  mt: PropTypes.number,
 };
