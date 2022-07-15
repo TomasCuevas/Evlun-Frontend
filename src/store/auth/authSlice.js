1e7d282596140a244eae6505f0dc1a522df22a30
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-15 15:19:07 -0300

    slice auth UPDATED: REMOVED isLoading property

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -4,27 +4,24 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState: {
     errorMessage: undefined,
-    isLoading: true,
     status: 'checking',
     user: {},
   },
   reducers: {
     onLogin: (state, { payload }) => {
       state.errorMessage = undefined;
-      state.isLoading = false;
       state.status = 'authenticated';
       state.user = payload;
     },
     onLogout: (state, { payload }) => {
       state.errorMessage = payload || undefined;
-      state.isLoading = false;
       state.status = 'not-authenticated';
       state.user = {};
     },
-    checkingCredentials: (state) => {
+    onCheckingCredentials: (state) => {
       state.status = 'checking';
     },
   },
 });
 
-export const { onLogin, onLogout, checkingCredentials } = authSlice.actions;
+export const { onLogin, onLogout, onCheckingCredentials } = authSlice.actions;
