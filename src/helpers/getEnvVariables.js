24e39a786e43cad2922a5b8fb7bb6dfb629decc7
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-19 15:55:00 -0300

    helper getEnvVariables UPDATED: ADDED a new variable export

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,6 +1,7 @@
 export const getEnvVariables = () => {
   return {
     VITE_API_AUTH_URL: import.meta.env.VITE_API_AUTH_URL,
+    VITE_API_USER_URL: import.meta.env.VITE_API_USER_URL,
     VITE_API_POST_URL: import.meta.env.VITE_API_POST_URL,
   };
 };
