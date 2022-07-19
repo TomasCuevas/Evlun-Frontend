f0d595c61eda58c4d4087af36a2c98e4f776a9c1
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-19 17:32:58 -0300

    redux store UPDATED: ADDED postsSlice

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,13 @@ import { configureStore } from '@reduxjs/toolkit';
  * @slices
  */
 import { authSlice } from './auth/authSlice';
+import { postsSlice } from './posts/postsSlice';
 import { profileSlice } from './profile/profileSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    posts: postsSlice.reducer,
     profile: profileSlice.reducer,
   },
 });
