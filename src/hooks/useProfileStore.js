e162a1e74fc0410586f99de88e71b528632b1dd2
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-19 17:35:59 -0300

    hook useProfileStore: ADDED

---
+++ src/hooks/useProfileStore.js
@@ -0,0 +1,43 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  onLoading,
+  onSetErrorMessage,
+  onSetPosts,
+  onSetUser,
+  onStopLoading,
+} from '../store/profile/profileSlice';
+import { postApi, userApi } from '../apis';
+
+export const useProfileStore = () => {
+  const { isLoading, user, errorMessage, posts } = useSelector((state) => state.profile);
+  const dispatch = useDispatch();
+
+  const startLoading = async (username) => {
+    try {
+      const newLt = new Date().getTime();
+      dispatch(onLoading(newLt));
+
+      const { data: userData } = await userApi(`?username=${username}`);
+      dispatch(onSetUser(userData.user));
+
+      const { data: postData } = await postApi(`?id=${userData.user._id}&lt=${newLt}`);
+      dispatch(onSetPosts(postData.posts));
+    } catch (error) {
+      console.log(error);
+      dispatch(onSetErrorMessage(error.response.data.msg));
+      dispatch(onStopLoading());
+    }
+  };
+
+  return {
+    // properties
+    ...user,
+    errorMessage,
+    isLoading,
+    posts,
+    user,
+
+    // methods
+    startLoading,
+  };
+};
