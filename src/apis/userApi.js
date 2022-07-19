ed161038198a5a191ff1c4a1cc8ca509febd5355
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-19 15:53:59 -0300

    axios userApi: ADDED

---
+++ src/apis/userApi.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { getEnvVariables } from '../helpers/getEnvVariables';
+
+const { VITE_API_USER_URL } = getEnvVariables();
+
+const userApi = axios.create({
+  baseURL: VITE_API_USER_URL,
+});
+
+userApi.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    'x-token': localStorage.getItem('token'),
+  };
+
+  return config;
+});
+
+export default userApi;
