14804c359c11294780887e59d53a5f9dbb8dd82b
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-19 18:23:21 -0300

    EvlunApp UPDATED: ADDED new routes and useAuthStore for checking authentication

--- src/EvlunApp.jsx
+++ src/EvlunApp.jsx
@@ -1,67 +1,57 @@
-import { useContext, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-
-/**
- * @context
- */
-import { AuthContext } from './context/AuthContext/AuthContext';
-
-/**
- * @providers
- */
-import { ProfileProvider } from './context/ProfileContext/ProfileContext';
 
 /**
  * @pages
  */
-import { ExplorePage } from './pages/ExplorePage/ExplorePage';
-import { LoginPage } from './pages/LoginPage/LoginPage';
-import { MainPage } from './pages/MainPage/MainPage';
-import { ProfilePage } from './pages/ProfilePage/ProfilePage';
-import { SettingsPage } from './pages/SettingsPages/SettingsPage';
-import { SignupPage } from './pages/SignupPage/SignupPage';
+import {
+  LoginPage,
+  MainPage,
+  SignupPage,
+  LoadingPage,
+  SettingsPage,
+  ExplorePage,
+  ProfilePage,
+} from './pages';
 
 /**
- * @fixtureData
+ * @routes
  */
-import { user } from './fixtures/user';
 import { SettingsRoutes } from './router/SettingsRoutes';
 
-export const EvlunApp = () => {
-  const { auth, loading } = useContext(AuthContext);
+/**
+ * @hooks
+ */
+import { useAuthStore } from './hooks/useAuthStore';
 
-  const { login } = useContext(AuthContext);
+export const EvlunApp = () => {
+  const { status, startChecking } = useAuthStore();
 
   useEffect(() => {
-    const onLogin = async () => {
-      login('abc123', user);
-    };
+    startChecking();
+  }, []);
 
-    onLogin();
-  }, [auth]);
+  if (status === 'checking') return <LoadingPage />;
 
   return (
     <>
-      {!loading && (
+      {status === 'not-authenticated' && (
         <Routes>
-          <Route path="/" element={auth ? <MainPage /> : <LoginPage />} />
-          <Route path="/signup" element={auth ? <Navigate to="/" /> : <SignupPage />} />
-          <Route path="/login" element={auth ? <Navigate to="/" /> : <LoginPage />} />
-          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/auth/signup" element={<SignupPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </Routes>
+      )}
 
+      {status === 'authenticated' && (
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route path="/explore" element={<ExplorePage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/settings/*" element={<SettingsRoutes />} />
-
-          <Route
-            path="/profile/:username"
-            element={
-              <ProfileProvider>
-                <ProfilePage />
-              </ProfileProvider>
-            }
-          />
-
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/*" element={<Navigate to="/" />} />
         </Routes>
       )}
     </>
