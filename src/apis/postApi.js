9e3b08eec54d764a8b6463d81d17a462f8cfd1a4
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-19 15:53:41 -0300

    axios postApi: ADDED

---
+++ src/apis/postApi.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { getEnvVariables } from '../helpers/getEnvVariables';
+
+const { VITE_API_POST_URL } = getEnvVariables();
+
+const postApi = axios.create({
+  baseURL: VITE_API_POST_URL,
+});
+
+postApi.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    'x-token': localStorage.getItem('token'),
+  };
+
+  return config;
+});
+
+export default postApi;
