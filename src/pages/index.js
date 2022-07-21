b466e1934851574a478e9eb8c58c4abee1d1740e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-21 18:08:27 -0300

    /pages index.js: UPDATED

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ export * from './ExplorePage/ExplorePage';
 export * from './LoadingPage/LoadingPage';
 export * from './LoginPage/LoginPage';
 export * from './MainPage/MainPage';
+export * from './PostPage/PostPage';
 export * from './ProfilePage/ProfilePage';
 export * from './SettingsPages/index';
 export * from './SignupPage/SignupPage';
