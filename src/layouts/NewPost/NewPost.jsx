a1d3e4359f026bee0d844de2e19d6c6f571d31ab
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-26 13:10:18 -0300

    layout NewPost: UPDATED

--- src/layouts/NewPost/NewPost.jsx
+++ src/layouts/NewPost/NewPost.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react';
-
 /**
  * @components
  */
-import { PostAvatarSection } from '../../components/Post/PostAvatarSection';
-
-/**
- * @hooks
- */
-import { useAuthStore } from '../../hooks/useAuthStore';
-import { useForm } from '../../hooks/useForm';
-import { usePostsStore } from '../../hooks/usePostsStore';
+import { AvatarSection, FormSection } from '../../components/NewPost';
 
 /**
  * @styles
@@ -18,51 +9,10 @@ import { usePostsStore } from '../../hooks/usePostsStore';
 import Styles from './NewPost.module.scss';
 
 export const NewPost = () => {
-  const { startCreateNewPosts, isCreating } = usePostsStore();
-  const { avatar, name, username } = useAuthStore();
-  const [height, setHeight] = useState(40);
-  const { post, onInputChange } = useForm({
-    post: '',
-  });
-
-  const onInput = (input) => {
-    const heightInput = input.target.scrollHeight;
-    setHeight(heightInput);
-  };
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    if (post.length < 1 || post.length > 155) return;
-
-    await startCreateNewPosts(post);
-    onInputChange({ target: { value: '', name: 'post' } });
-  };
-
   return (
     <article className={Styles.newpost__container}>
-      <section className={Styles.top__section}>
-        <PostAvatarSection avatar={avatar} username={username} name={name} />
-        <div className={Styles.input__section}>
-          <textarea
-            className={Styles.input}
-            disabled={isCreating}
-            maxLength={155}
-            name="post"
-            onChange={onInputChange}
-            onInput={onInput}
-            placeholder="¿Qué está pasando?"
-            style={{ height }}
-            value={post}
-          />
-        </div>
-      </section>
-      <section className={Styles.botton__section}>
-        <div className={Styles.button__container}>
-          <button type="submit" disabled={isCreating} className={Styles.button} onClick={onSubmit}>
-            Postear
-          </button>
-        </div>
-      </section>
+      <AvatarSection />
+      <FormSection />
     </article>
   );
 };
