3be08c264c057c31dbe6d6a939d220e35c1a7983
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-27 15:46:09 -0300

    layout Post: UPDATED

--- src/layouts/Post/Post.jsx
+++ src/layouts/Post/Post.jsx
@@ -1,33 +1,23 @@
-import PropTypes from 'prop-types';
-
 /**
  * @components
  */
-import { PostAvatarSection, PostBottom, PostContent, PostTop } from '../../components/Post';
+import { AvatarSection, Bottom, Content, Top } from '../../components/Post';
 
 /**
  * @styles
  */
-import Styles from './post.module.scss';
+import Styles from './Post.module.scss';
 
-export const Post = ({ post }) => {
+export const Post = () => {
   return (
     <article className={Styles.post__container}>
-      <PostAvatarSection
-        avatar={post.added_by.avatar}
-        name={post.added_by.name}
-        username={post.added_by.username}
-      />
+      <AvatarSection />
 
       <div className={Styles.post__section}>
-        <PostTop name={post.added_by.name} username={post.added_by.username} date={post.date} />
-        <PostContent _id={post._id} content={post.content} />
-        <PostBottom postId={post._id} likes={post.likes} comments={post.comments} />
+        <Top />
+        <Content />
+        <Bottom />
       </div>
     </article>
   );
-};
-
-Post.propTypes = {
-  post: PropTypes.object.isRequired,
 };
