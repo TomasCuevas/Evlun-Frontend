d8ff603fbaf6f34820e5d9e3a7e2cbde4f237bc1
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-27 15:45:33 -0300

    layout FeedPostHome UPDATED: ADDED context to the children

--- src/layouts/FeedPostsHome/FeedPostsHome.jsx
+++ src/layouts/FeedPostsHome/FeedPostsHome.jsx
@@ -4,6 +4,11 @@
 import { Post } from '../';
 
 /**
+ * @pages
+ */
+import { LoadingPage } from '../../pages/LoadingPage/LoadingPage';
+
+/**
  * @styles
  */
 import Styles from './FeedPostsHome.module.scss';
@@ -13,14 +18,23 @@ import Styles from './FeedPostsHome.module.scss';
  */
 import { usePostsStore } from '../../hooks/usePostsStore';
 
+/**
+ * @context
+ */
+import { PostContext } from '../../context';
+
 export const FeedPostsHome = () => {
-  const { posts } = usePostsStore();
+  const { posts, isLoading } = usePostsStore();
+
+  if (isLoading) return <LoadingPage />;
 
   return (
     <section className={Styles.feedposts__container}>
       <div className={Styles.allposts__container}>
         {posts.map((post) => (
-          <Post key={post._id} post={post} />
+          <PostContext.Provider key={post._id} value={{ ...post }}>
+            <Post />
+          </PostContext.Provider>
         ))}
       </div>
     </section>
